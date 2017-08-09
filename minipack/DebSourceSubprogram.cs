6c4d4e2b12810abf45e08ad86e72e2a45979b852
jonathanvdc <jonathan.vdc@outlook.com>
2017-08-09 18:50:47 +0200

    Check deb-source input args

--- minipack/DebSourceSubprogram.cs
+++ minipack/DebSourceSubprogram.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Flame.Compiler;
+using Flame.Front.Options;
 
 namespace Minipack
 {
@@ -26,7 +28,68 @@ namespace Minipack
         /// <inheritdoc/>
         public override void Run(IReadOnlyList<string> args, ICompilerLog log)
         {
+            var optParser = StringOptionParser.CreateDefault();
+            var options = BuildArguments.Parse(optParser, args.ToArray<string>());
+            log = new OptionLog(log, options);
+
+            if (options.SourcePaths.Length != 1)
+            {
+                LogInvalidSourcePathArity(log, options.SourcePaths.Length);
+                return;
+            }
+
+            string version = options.GetOption<string>("version", null);
+            if (string.IsNullOrWhiteSpace(version))
+            {
+                LogMissingMandatoryOption(log, "--version");
+                return;
+            }
+
+            string revision = options.GetOption<string>("revision", null);
+            if (string.IsNullOrWhiteSpace(revision))
+            {
+                LogMissingMandatoryOption(log, "--revision");
+                return;
+            }
+
+            string outputDir = options.GetOption<string>("o", null);
+            if (string.IsNullOrWhiteSpace(outputDir))
+            {
+                LogMissingMandatoryOption(log, "-o");
+                return;
+            }
+
             throw new NotImplementedException();
+        }
+
+        private static void LogMissingMandatoryOption(ICompilerLog log, string optionName)
+        {
+            log.LogError(
+                new LogEntry(
+                    "missing mandatory option",
+                    string.Format(
+                        "the mandatory '{0}' option is missing.",
+                        optionName)));
+        }
+
+        private static void LogInvalidSourcePathArity(ICompilerLog log, int arity)
+        {
+            if (arity > 1)
+            {
+                log.LogError(
+                    new LogEntry(
+                        "too many input files",
+                        string.Format(
+                            "exactly one input file was expected, but {0} were provided.",
+                            arity)));
+            }
+            else
+            {
+                log.LogError(
+                    new LogEntry(
+                        "no input file",
+                        "exactly one input file must be provided."));
+            }
         }
     }
 }
